5dddb98d3c48e898b580abad9e871c24d273c6fc
mfouesneau <fouesneau@mpia.de>
2021-03-11 12:49:59 +0000

    Example using a conan installed xtensor

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include "xtensor/xarray.hpp"
+#include "xtensor/xio.hpp"
+#include "xtensor/xview.hpp"
 #include "example.hpp"
 
 int main() {
@@ -10,4 +13,16 @@ int main() {
     std::cout << "\n";
 
     example::example1();
+
+    xt::xarray<double> arr1
+    {{1.0, 2.0, 3.0},
+    {2.0, 5.0, 7.0},
+    {2.0, 5.0, 7.0}};
+
+    xt::xarray<double> arr2
+    {5.0, 6.0, 7.0};
+
+    xt::xarray<double> res = xt::view(arr1, 1) + arr2;
+
+    std::cout << res;
 }
