c162b63418916f8f209818e29b5c4783b7261a15
Morgan Fouesneau <mfouesneau@users.noreply.github.com>
2021-03-12 11:57:18 +0100

    Update mplpp_example.cpp

--- mplpp_example.cpp
+++ mplpp_example.cpp
@@ -8,6 +8,10 @@ int main() {
         {16, 5, 9, 4}, {2, 11, 7, 14}, {3, 10, 6, 15}, {13, 8, 12, 1}};
     plot(Y);
 
-    show();
+    //show();  // does not work on docker systems without DISPLAY
+    // you may need to set GNUTERM=png or something without a display
+    save("img/mplexample_fig.png");
+    save("img/mplexample_fig.pdf");
+    save("img/mplexample_fig.tex");
     return 0;
 }
